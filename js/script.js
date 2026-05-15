279d60956ede2197272c602532502b37e046ca36
Al-Ghazi <umer.awan123@hotmail.com>
2026-05-15 01:04:15 +0100

    2nd Last

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const mobileLinks = document.querySelectorAll(".mobile-nav a");
+console.log(mobileLinks);
+
+function toggleMenu() {
+    document.getElementById("mobileNav").classList.toggle("active");
+}
+
+mobileLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        document.getElementById("mobileNav").classList.remove("active");
+    });
+});
